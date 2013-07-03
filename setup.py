4ae353b59dcda24557fe47299556933f1d63321a
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2013-07-03 11:03:01 +0200

    Missing meta-data to register as package

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 setup(
+    author = "kliment",
+    author_email = "kliment@mail.com",
+    maintainer = "Mekza",
+    maintainer_email = "martin@mekkaoui.fr",
+    url = "https://github.com/paymill/paymill-python",
     name='pymill',
     version='0.1.2',
     description='Python API for Paymill credit card processing service',
